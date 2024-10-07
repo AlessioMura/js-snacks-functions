be5af59a2508626cc223d2bc82eb0be8df9ea451
AlessioMura <alessiomura03@gmail.com>
2024-10-07 16:14:25 +0200

    ex2- Filtra Iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,9 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function my_func(arr, first_letter) {
+    const output = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].startsWith(first_letter)) {
+            output.push(arr[i]);
+        }
+        
+    }
+    return output;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+let first_letter_a = my_func(names, 'A');
+console.log(first_letter_a);
 
 
 
