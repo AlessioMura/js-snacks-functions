8ddc02fde1a59dfc75c4c5d3518989e44ddc5c9f
AlessioMura <alessiomura03@gmail.com>
2024-10-07 16:29:56 +0200

    ex3- Conta Vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function my_func(string) {
+    let vowels = 'aeiou';
+    let vowels_count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.indexOf(string[i]) !== -1) {
+            vowels_count += 1;
+        }
+    }
+    return vowels_count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let total_vowels = my_func(word);
+console.log(total_vowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
